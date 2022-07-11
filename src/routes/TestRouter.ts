5f9f590ec52764234caf15e96d9bf36966995b3a
Hyosik Philip Joo <lux1937@gmail.com>
2022-07-11 19:05:59 +0900

    [FEAT] Route Subschedule

--- src/routes/TestRouter.ts
+++ src/routes/TestRouter.ts
@@ -4,5 +4,7 @@ import TestController from "../controllers/TestController";
 const router: Router = Router();
 
 router.get('/date', TestController.testDate);
+router.post('/self-reference', TestController.testSelfReference);
+router.post('/subschedule', TestController.createSubSchedule);
 
 export default router;
