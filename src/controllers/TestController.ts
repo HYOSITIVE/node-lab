85f4b5e18c1f1d5fa5cec21486b9b2f278298548
Hyosik Philip Joo <lux1937@gmail.com>
2022-07-11 18:13:19 +0900

    [TEMP] Implement temporary Date-String conversion

--- src/controllers/TestController.ts
+++ src/controllers/TestController.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import util from "../modules/util";
 import message from "../modules/responseMessage";
 import statusCode from "../modules/statusCode";
+import { MovieService } from "../services";
 
 /**
  * @route GET /date
@@ -15,6 +16,9 @@ const testDate = async (req: Request, res: Response) => {
     const dateDate = new Date(stringDate);
     console.log(`stringDate: ${typeof(stringDate)} ${stringDate}`);
     console.log(`dateDate: ${typeof(dateDate)} ${dateDate}`);
+
+    // createMovie 호출해서 stringDate 들어가는지 확인
+    
     res.status(statusCode.OK).send(util.success(statusCode.OK, message.SEARCH_MOVIE_SUCCESS));
 }
 
