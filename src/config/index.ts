be30dbc9a5e049b7eaff8ed6d70a4c0f31203a3d
Hyosik Philip Joo <lux1937@gmail.com>
2023-01-02 12:34:50 +0900

    [ADD] Install pg, typeorm

--- src/config/index.ts
+++ src/config/index.ts
@@ -26,15 +26,23 @@ export default {
    */
   jwtSecret: process.env.JWT_SECRET as string,
 
-   /**
+  /**
    * jwt Algorithm
    */
   jwtAlgo: process.env.JWT_ALGO as string,
 
-   /**
+  /**
    * AWS S3
    */
   s3AccessKey: process.env.S3_ACCESS_KEY as string,
   s3SecretKey: process.env.S3_SECRET_KEY as string,
   bucketName: process.env.BUCKET_NAME as string,
+
+  /**
+   * PostgreSQL + TypeORM
+   */
+  pgHost: process.env.PG_HOST as string,
+  pgUser: process.env.PG_USER as string,
+  pgPassword: process.env.PG_PASSWORD as string,
+  pgDatabase: process.env.PG_DATABASE as string,
 };
