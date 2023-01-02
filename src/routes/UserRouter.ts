c4fff292ee6fdc326c6b8fb81d90243266185ac2
Hyosik Philip Joo <lux1937@gmail.com>
2023-01-02 14:32:29 +0900

    [FEAT] Test typeorm create user

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -3,21 +3,30 @@ import { UserController } from "../controllers";
 import { body } from "express-validator/check";
 const router: Router = Router();
 
-router.post('/', UserController.createUser);
-router.put('/:userId', UserController.updateUser);
-router.get('/:userId', UserController.findUserById);
-router.delete('/:userId', UserController.deleteUser);
-router.post('/', [
-    body('email').isEmail(),
-    body('password').isLength({ min: 6 }),
-    body('name').notEmpty(),
-    body('phone').notEmpty()
-], UserController.createUser);
-router.post('/signin', [
-    body('email').notEmpty(),
-    body('email').isEmail(),
-    body('password').isLength({ min: 6 }),
-    body('password').notEmpty()    
-], UserController.signInUser);
+router.post("/", UserController.createUser);
+router.post("/pg", UserController.createPgUser);
+router.put("/:userId", UserController.updateUser);
+router.get("/:userId", UserController.findUserById);
+router.delete("/:userId", UserController.deleteUser);
+router.post(
+  "/",
+  [
+    body("email").isEmail(),
+    body("password").isLength({ min: 6 }),
+    body("name").notEmpty(),
+    body("phone").notEmpty(),
+  ],
+  UserController.createUser
+);
+router.post(
+  "/signin",
+  [
+    body("email").notEmpty(),
+    body("email").isEmail(),
+    body("password").isLength({ min: 6 }),
+    body("password").notEmpty(),
+  ],
+  UserController.signInUser
+);
 
 export default router;
